8b56eb3746108327ecd411a9b52971af45922bb4
KevinCA <nivekcuevas.kca@gmail.com>
2025-11-27 04:13:43 -0600

    Validacion de formulario y animacion de teclado

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -2,4 +2,5 @@ import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   // Aquí pondremos la lógica de validación pronto
+  final phoneNumber = ''.obs;
 }
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -15,24 +15,48 @@ class LoginView extends GetView<LoginController> {
 
   @override
   Widget build(BuildContext context) {
+    // Definimos si el teclado está abierto
+    bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Stack(
         fit: StackFit.expand,
         children: [
           //Imagen de fondo con filtro de color - FIJA
-            Image.asset(_backgroundImagePath, fit: BoxFit.cover),
+          Image.asset(_backgroundImagePath, fit: BoxFit.cover),
           //Contenido de la pantalla
           SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: const [
-                  //Espacio en blanco arriba
-                  SizedBox(height: 80),
-                  //Logo de la app
-                  _LoginCard(),
-                ],
-              ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                    ),
+                    child: Column(
+                      children: [
+                        //Espacio en blanco arriba
+                        AnimatedContainer(
+                          duration: const Duration(
+                            milliseconds: 300,
+                          ), // Cuánto tarda la animación
+                          height: isKeyboardOpen
+                              ? 0
+                              : 80, // <--- AQUÍ ESTÁ TU TRUCO MAESTRO
+                          child: const SizedBox(), // Contenido vacío
+                        ),
+                        //Card con elementos de login
+                        _LoginCard(),
+                      ],
+                    ),
+                  ),
+                ),
+                Text(
+                  'Creada por KevinCa © Nov/2025',
+                  style: TextStyle(color: Colors.white70, fontSize: 12),
+                ),
+              ],
             ),
           ),
         ],
@@ -78,7 +102,7 @@ class _LoginCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
       margin: const EdgeInsets.symmetric(horizontal: 20),
       decoration: BoxDecoration(
         color: Colors.black26,
@@ -90,16 +114,18 @@ class _LoginCard extends StatelessWidget {
             offset: const Offset(0, 3),
           ),
         ],
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(15),
       ),
       width: double.infinity,
       child: Column(
         children: [
+          //Logo de la app
           const CircleAvatar(
             backgroundImage: AssetImage(LoginView._logoImagePath),
             radius: 50,
           ),
           const SizedBox(height: 16),
+          //Titulo y subtitulo
           Text(
             LoginView._titleText,
             style: _titleStyle,
@@ -112,16 +138,54 @@ class _LoginCard extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 16),
-          Form(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: _inputDecoration,
+          //Formulario de login
+          _LoginForm(inputDecoration: _inputDecoration),
+          const SizedBox(height: 16),
+          //Botón de ingresar
+          TextButton(
+            onPressed: () => {
+              //TODO: GO HOME despues de validar
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.purple,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 40),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
               ),
             ),
+            child: Text('Ingresar'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({required InputDecoration inputDecoration})
+    : _inputDecoration = inputDecoration;
+
+  final InputDecoration _inputDecoration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TextFormField(
+          keyboardType: TextInputType.phone,
+          decoration: _inputDecoration,
+          validator: (value) {
+            String pattern =
+                r'^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$';
+            RegExp regExp = RegExp(pattern);
+            return (value != null && regExp.hasMatch(value))
+                ? null
+                : 'Número de teléfono inválido';
+          },
+        ),
       ),
     );
   }
